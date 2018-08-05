7a896723ee6b5cc244fac4e7a100eb588fe8c1aa
Khari Woods <khariwoods3@gmail.com>
2018-08-05 01:45:59 -0400

    First draft of new project

---
+++ assets/JS/effects.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+	$('.menuIcon').click(function(){
+	$('.mobileContent').slideToggle(400,'swing');
+	
+})
+
+
+// $('.product').hover(
+// 			function(){$(this).find('.product_summary:first').slideToggle(300,'swing');},
+// 			function(){$(this).find('.product_summary:first').fadeOut(800);})
+// //Add slide show function
+
+});
